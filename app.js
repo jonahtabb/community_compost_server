760e17a24ab46a5d98aa9930f1e3c1628d4021d6
jonahtabb <jtabbdev@gmail.com>
2021-09-12 22:47:37 -0400

    middleware updates and bug fixes

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ require('dotenv').config()
 const express = require("express");
 const controllers = require('./controllers');
 const app = express();
+const cors = require('cors')
 
 startApp = async () => {
     
     app.use(express.json())
-    
+    app.use(cors())
+    app.options('*', cors())
     app.use("/user", controllers.userController)
     app.use("/member", controllers.memberController)
     app.use("/admin", controllers.adminController)
     app.use("/community", controllers.communityController)
     app.use("/group", controllers.pickupGroupsController)
-    app.listen(3000, () => {
-        console.log(`[Server]: App is listening on 3000.`);
+    
+    app.listen(process.env.PORT, () => {
+        console.log(`[Server]: App is listening on ${process.env.PORT}.`);
     });
 }
 
