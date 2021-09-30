97e5e64af5876181586d7a6fe24589fc1ad377ca
jonahtabb <jtabbdev@gmail.com>
2021-09-29 23:18:49 -0400

    config debugging

--- db.js
+++ db.js
@@ -3,16 +3,17 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize(
     process.env.DB_DBNAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    encodeURIComponent(process.env.DB_PASS),
     {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         ssl: process.env.ENVIRONMENT === 'production'
     }
 )
-console.log()
+console.log('************', process.env.ENVIRONMENT)
 
 async function syncDb(sequelize, options){
+    console.info('************', process.env.ENVIRONMENT)
     const {force, alter} = options
     try {
         if (force){
